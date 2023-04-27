44e6d663dec6a70677b40cd4f50b3a960fe7a0b4
unknown <hhu_zyh@qq.com>
2023-04-27 21:12:09 +0800

    4.27.24

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -9,7 +9,7 @@ except ImportError as e:
     import numpy as np
 
 def ridge_train(x, y):
-    lbda = 0.1
+    lbda = 1
     I = np.eye(x.shape[1])
     res = np.dot(x.T,x) + lbda * I
     res_inv = np.linalg.inv(res)
@@ -25,7 +25,7 @@ def ridge(data):
 def lasso_train(x, y):
     m, n = x.shape
     lbda = 0.1
-    theta = np.zeros(n, 1) 
+    theta = np.zeros((n, 1)) 
     for i in range(100000):
         theta -= lbda * np.sign(theta) + np.dot(x.T, np.dot(x, theta) - y) / m
         theta[np.abs(theta) < lbda] = 0
